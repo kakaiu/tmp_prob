7be6ead29b5d665025501e7c572dea287a23b772
Zhe Wang <zhewang.kakaiu@yahoo.com>
2021-02-28 23:10:08 -0600

    z

--- prob3.py
+++ prob3.py
@@ -68,7 +68,7 @@ def proc(c, d):
 	for i in range(n_trials):
 		if trial(n, f, c, d) == True:
 			count = count + 1
-	print count
+	print count, n_trials
 
 if __name__ == "__main__":
 	n = int(sys.argv[1])
